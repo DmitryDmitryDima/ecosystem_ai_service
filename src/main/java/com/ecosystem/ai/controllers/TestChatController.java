3aa31f365940d9c5df1e9cb519e42cedc90c1ba3
User <astakamokro@yandex.ru>
2025-11-08 18:31:07 +0300

    init: experimenting with ollama

--- src/main/java/com/ecosystem/ai/controllers/TestChatController.java
+++ src/main/java/com/ecosystem/ai/controllers/TestChatController.java
@@ -1,6 +1,7 @@
 package com.ecosystem.ai.controllers;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,12 +29,22 @@ public class TestChatController {
     }
 
     // стримим результат дял лучшего пользовательского опыта
+    // метод doOnNext позволяет перехватывать ответ. Сюда можно вставить event listener
     @GetMapping("/stream")
     public Flux<String> stream(){
         return chatClient.prompt()
                 .user("дай самое краткое определение комплекса гольджи. Ответ пришли в json формате {suggestion:your_answer}")
                 .stream()
-                .content();
+                .content().doOnNext(System.out::println);
 
+    }
+
+
+    @GetMapping("/chatResponse")
+    public ChatResponse chatResponse(){
+        return chatClient.prompt("дай самое краткое определение комплекса гольджи." +
+                        " Ответ пришли в json формате {suggestion:your_answer}")
+                .call()
+                .chatResponse();
     }
 }
